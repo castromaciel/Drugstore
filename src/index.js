08ea26a4202a981cc1ce2bf66137ea8a8fdd7cf8
Ramiro <raminieva067sk@gmail.com>
2021-10-27 22:20:19 -0300

    se agrego el footer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Footer from './components/Footer/Footer'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Footer />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
