d93a0d66089bcb79984a7384bcc8f51e2a6cb199
Maciel Castro <castromaciel00@gmail.com>
2021-11-02 15:31:04 -0300

    Se modificó el mensaje de error en el login y el registro, cuando una clave o correo son incorrectos

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import './register.css'
 
 function Register() {
-
+  
+  const[registerErrors,setRegisterErrors] = useState([])
   const {register, handleSubmit, formState: {errors}, getValues  } = useForm({ defaultValues: { username:"", email:"", emailConfirmation:"", password:"", passwordConfirmation:""}})
 
   const onSubmit = data => {
@@ -21,29 +22,31 @@ function Register() {
     })
       .then(res => res.json())
       .then(json => {
-        console.log(json)
+        setRegisterErrors(json.errors)
       })  
-    
-    // console.log(data)
-    // alert('Usuario Registrado')
-    // reset()
+   
   }
-
   return (
     <div>
       <form className="container" onSubmit={handleSubmit(onSubmit)}>
         <div className="mx-auto pt-2 form-group">
           <h5 className="ps-2">Username</h5>
           <input type="text" className="form-control" placeholder="Username" name="username" {...register("username", {required: true, minLength:5, maxLength:15})} />
-          {errors?.username?.type === "required" && <span>⚠ This field is required</span>}
-          {errors?.username?.type === "minLength" && <span>⚠ Your username must be at least 5 characters long</span>}
+          {errors?.username?.type === "required" && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>This field is required</span>}
+          {errors?.username?.type === "minLength" && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>Your username must be at least 5 characters long</span>}
+          { registerErrors.map(error => 
+            (error?.param === 'username' && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>{error.msg}</span>))
+          }
         </div>
         <div className="d-flex flex-column flex-lg-row">
           <div className="me-lg-2 pt-2 form-group w-100">
             <h5 className="ps-2">Email address</h5>
             <input type="text" className="form-control" placeholder="example@example.com" name="email" {...register("email", {required: true, pattern:/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/})} />
-            {errors?.email?.type === "required" && <span>⚠ This field is required</span>}
-            {errors?.email?.type === 'pattern' && <span>⚠ Insert a valid mail</span> }
+            {errors?.email?.type === "required" && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>This field is required</span>}
+            {errors?.email?.type === 'pattern' && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>Insert a valid mail</span> }
+            { registerErrors.map(error => 
+              (error?.param === 'email' && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>{error.msg}</span>))
+            }
           </div>
           <div className="ms-lg-2 pt-2 form-group w-100">
             <h5 className="ps-2">Confirm your email</h5>
@@ -52,20 +55,21 @@ function Register() {
                 matchesPreviousEmail: (value) => 
                 {
                   const { email } = getValues();
-                  return email === value || '⚠ Emails should match!'
+                  return email === value || <i className="bi bi-exclamation-octagon-fill me-2"></i>
                 }
               },})} />
-            {errors?.emailConfirmation?.type === "required" && <span>⚠ This field is required</span>}
-            {errors?.email?.type === 'pattern' && <span>⚠ Insert a valid mail</span> }
-            {errors.emailConfirmation && <span>{errors.emailConfirmation.message}</span>}
+            {errors?.emailConfirmation?.type === "required" && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>This field is required</span>}
+            {errors?.email?.type === 'pattern' && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>Insert a valid mail</span> }
+            {errors.emailConfirmation && <span>{errors.emailConfirmation.message}Email should match!</span>}
+            
           </div>
         </div>
         <div className="d-flex flex-column flex-lg-row">
           <div className="me-lg-2 pt-2 form-group w-100">
             <h5 className="ps-2">Password</h5>
             <input type="password" className="form-control" placeholder="password" name="password" {...register("password", {required: true, minLength:8})} />
-            {errors?.password?.type === "required" && <span>⚠ This field is required</span>}
-            {errors?.password?.type === "minLength" && <span>⚠ Password must be at least 8 characters long</span>}
+            {errors?.password?.type === "required" && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>This field is required</span>}
+            {errors?.password?.type === "minLength" && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>Password must be at least 8 characters long</span>}
           </div>
           <div className="ms-lg-2 pt-2 form-group w-100">
             <h5 className="ps-2">Confirm your password</h5>
@@ -75,13 +79,13 @@ function Register() {
               {
                 matchesPreviousPassword: (value) => 
                 { const { password } = getValues();
-                  return password === value || '⚠ Passwords should match!'
+                  return password === value || <i className="bi bi-exclamation-octagon-fill me-2"></i>
                 }
               },
             })} />
-            {errors?.passwordConfirmation?.type === "required" && <span>⚠ This field is required</span>}
-            {errors?.passwordConfirmation?.type === "minLength" && <span>⚠ Password must be at least 8 characters long</span>}
-            {errors.passwordConfirmation && <span>{errors.passwordConfirmation.message}</span>}
+            {errors?.passwordConfirmation?.type === "required" && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>This field is required</span>}
+            {errors?.passwordConfirmation?.type === "minLength" && <span><i className="bi bi-exclamation-octagon-fill me-2"></i>Password must be at least 8 characters long</span>}
+            {errors.passwordConfirmation && <span>{errors.passwordConfirmation.message}Password should match!</span>}
           </div>
         </div>
         <div className="mx-auto pt-2 form-group d-flex justify-content-end">
