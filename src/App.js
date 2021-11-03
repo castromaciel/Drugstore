6e15ca5d274848fe9cabcae105e277c9b2a9feb2
Jacqueline Michelle Paredes <jacquelinep_@hotmail.com>
2021-11-03 13:02:11 -0300

    Se agregó el banner (#5)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Form from "./components/Form/Form";
+import Ban from "./components/Ban/Ban"
+import Navbar from "./components/Nav/Navbar"
 import ListCards from "./components/ListCards/ListCards"
 
 function App() {
   return (
     <div className="App"> 
-      <Form />
+    <Ban/>
+    <Navbar/>
       <ListCards />
     </div>
   );
