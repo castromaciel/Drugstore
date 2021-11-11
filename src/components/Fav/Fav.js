ec42ce227c9bdc485a98caa20c93eb95e6953a00
jaqomp <jacquelinep_@hotmail.com>
2021-11-11 14:58:44 -0300

    Se agregó el componente Fav

---
+++ src/components/Fav/Fav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Fav.css'
+import Navbar from '../Nav/Navbar'
+import '../Nav/Navbar.css'
+import Footer from '../Footer/Footer'
+import '../Footer/Footer'
+
+function Fav() {
+    return (
+        <div>
+            <Navbar/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Fav
