57c3fe40646a7cbec3d876538d50880f452201d4
jaqomp <jacquelinep_@hotmail.com>
2021-11-11 15:03:05 -0300

    Se agrego el componente Cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Cart.css'
+import Navbar from '../Nav/Navbar'
+import '../Nav/Navbar.css'
+import Footer from '../Footer/Footer'
+import '../Footer/Footer'
+function Cart() {
+    return (
+        <div>
+            <Navbar/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Cart
