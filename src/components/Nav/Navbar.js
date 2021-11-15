1d296cc34fc90aa0acc4dd70f3c6f276c393aee9
Ramiro <raminieva067sk@gmail.com>
2021-11-14 23:26:14 -0300

    se agrego el carousel

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -6,42 +6,6 @@ import { NavLink } from 'react-router-dom'
 import UserLogged from '../UserLogged/UserLogged'
 
 function Navbar() {
-<<<<<<< HEAD
-    return (
-        <div>
-          <nav className="navbar navbar-expand-lg navbar-light background-navbar">
-            <div className="collapse navbar-collapse background-navbar" id="navbarNav">
-              <ul className="navbar-nav m-4 background-navbar">
-                <li className="item-navbar">
-          <a className="nav-link active" aria-current="page" href="/#">Home</a>
-        </li>
-        <li className="item-navbar">
-          <a className="nav-link active" href="/#">Features</a>
-        </li>
-        <li className="item-navbar">
-          <a className="nav-link active" href="/#">Products</a>
-        </li>
-        <li className="item-navbar">
-        <a className="nav-link active" href="/#">Contact</a>
-        </li>
-      </ul>
-    </div>
-    <div className=" right-icons" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item i-right-icons">
-          <a className="nav-link" href="http://www.facebook.com"><i className="bi bi-star-fill"></i></a>
-        </li>
-        <li className="nav-item i-right-icons">
-          <a className="nav-link" href="http://www.twitter.com"><i className="bi bi-cart-fill"></i></a>
-        </li>
-        <li className="nav-item i-right-icons">
-          <a className="nav-link" href="http://www.instagram.com"><i className="bi bi-question-circle-fill"></i></a>
-        </li>
-        </ul>
-    </div>
-    <Form/>
-</nav>
-=======
 
   const [isLogged,setIsLogged] = useState(false)
   const token = localStorage.getItem('token')
@@ -70,7 +34,6 @@ function Navbar() {
               <a className="nav-link active" href="/#"><i className="bi bi-briefcase-fill pe-1"></i>Contacto</a>
             </li>
           </ul>
->>>>>>> 4fe65a1d7159f4d90f582bc85e7574723d4072c5
         </div>
         <div className="right-icons ms-3">
           <ul className="navbar-nav d-flex flex-row">
