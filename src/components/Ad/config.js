ca0afd082d31dde24e61a649a73d0de8eb9aed89
cajimenez96 <83433161+cajimenez96@users.noreply.github.com>
2021-11-16 10:51:46 -0300

    se agrega seccion publicidad (#19)
    Co-authored-by: Castro Maciel <83432755+castromaciel@users.noreply.github.com>

---
+++ src/components/Ad/config.js
@@ -0,0 +1,8 @@
+export const baseUrl =
+  process.env.NODE_ENV === "../../assets/img/ads"
+
+
+//   export const baseUrl =
+//   process.env.NODE_ENV === "production"
+//     ? "https://s3.amazonaws.com/static.neostack.com/img/react-slick"
+//     : "/img/react-slick";  
